a1948aa3a4fb8eb02fa45f8e035bed5e8f308cd8
Cotyforets <cotyforets@gmail.com>
2023-12-03 18:03:59 -0300

    cambios esteticos a la pagina fron end de crud

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 document. getElementById("header").innerHTML=`    <nav class="navbar navbar-expand-sm navbar-light bg-light">
 <div class="container">
-  <a class="navbar-brand" href="index.html">Formula 1</a>
+  <a class="navbar-brand" href="index.html"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/F1.svg/120px-F1.svg.png" alt="">
+  </a>
   <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
       aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
